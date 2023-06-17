6a394e87d764d17a921adac44ed9d0c5600ac608
Elhameed <teniolaajani2004@gmail.com>
2023-06-17 23:49:14 +0100

    revamp in progress

--- js/script.js
+++ js/script.js
@@ -1,94 +1,99 @@
+// Menu toggle functionality
 let menu = document.querySelector('#menu-bars');
 let header = document.querySelector('header');
 
-menu.onclick = () =>{
+// Toggle menu bars and header when menu is clicked
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     header.classList.toggle('active');
 }
 
-window.onscroll = () =>{
+// Remove menu bars and active header when scrolling
+window.onscroll = () => {
     menu.classList.remove('fa-times');
     header.classList.remove('active');
 }
 
+// Cursor positioning based on mouse movement
 let cursor1 = document.querySelector('.cursor-1');
 let cursor2 = document.querySelector('.cursor-2');
 
-window.onmousemove = (e) =>{
+window.onmousemove = (e) => {
     cursor1.style.top = e.pageY + 'px';
     cursor1.style.left = e.pageX + 'px';
     cursor2.style.top = e.pageY + 'px';
     cursor2.style.left = e.pageX + 'px';
 }
 
-document.querySelectorAll('a').forEach(links =>{
-
-    links.onmouseenter = () =>{
+// Cursor activation on link hover
+document.querySelectorAll('a').forEach(links => {
+    links.onmouseenter = () => {
         cursor1.classList.add('active');
         cursor2.classList.add('active');
     }
-
-    links.onmouseleave = () =>{
+    links.onmouseleave = () => {
         cursor1.classList.remove('active');
         cursor2.classList.remove('active');
     }
-
 });
 
-
+// Typing effect
 const typedTextSpan = document.querySelector(".typed-text");
-    const cursorSpan = document.querySelector(".cursor");
+const cursorSpan = document.querySelector(".cursor");
+const textArray = ["Front-end Developer", "Web Designer", "Web Developer", "Software Engineer"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
 
-    const textArray = ["Front-end Developer", "Web Designer", "Web Developer", "Software Engineer"];
-    const typingDelay = 200;
-    const erasingDelay = 100;
-    const newTextDelay = 2000; // Delay between current and next text
-    let textArrayIndex = 0;
-    let charIndex = 0;
-
-    function type() {
-      if (charIndex < textArray[textArrayIndex].length) {
-        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+// Type function for typing effect
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
         typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
         charIndex++;
         setTimeout(type, typingDelay);
-      } 
-      else {
+    } else {
         cursorSpan.classList.remove("typing");
         setTimeout(erase, newTextDelay);
-      }
     }
+}
 
-    function erase() {
-        if (charIndex > 0) {
-        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+// Erase function for typing effect
+function erase() {
+    if (charIndex > 0) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
         charIndex--;
         setTimeout(erase, erasingDelay);
-      } 
-      else {
+    } else {
         cursorSpan.classList.remove("typing");
         textArrayIndex++;
-        if(textArrayIndex>=textArray.length) textArrayIndex=0;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
         setTimeout(type, typingDelay + 1100);
-      }
     }
+}
 
-    document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-      if(textArray.length) setTimeout(type, newTextDelay + 250);
-    });
+// Start typing effect when DOM content is loaded
+document.addEventListener("DOMContentLoaded", function() {
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
 
-    const showOnPx = 150;
-const backToTopButton = document.querySelector(".back-to-top")
+// Back-to-top button functionality
+const showOnPx = 150;
+const backToTopButton = document.querySelector(".back-to-top");
 
+// Get the scroll container
 const scrollContainer = () => {
-  return document.documentElement || document.body;
+    return document.documentElement || document.body;
 };
 
+// Show or hide back-to-top button based on scroll position
 document.addEventListener("scroll", () => {
-  if (scrollContainer().scrollTop > showOnPx) {
-    backToTopButton.classList.remove("hidden")
-  } else {
-    backToTopButton.classList.add("hidden")
-  }
-})
+    if (scrollContainer().scrollTop > showOnPx) {
+        backToTopButton.classList.remove("hidden");
+    } else {
+        backToTopButton.classList.add("hidden");
+    }
+});
